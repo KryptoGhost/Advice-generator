c4b45dbc59362a4bb5e8ea770262e12e2bdd2e6f
KryptoGhost <106913123+KryptoGhost@users.noreply.github.com>
2023-02-10 12:59:40 +0100

    commit work

---
+++ app.js
@@ -0,0 +1,30 @@
+//use async to return a promise using try and catch
+
+
+const adviceNum = document.getElementById("addy");
+const adivce = document.querySelector(".advice");
+const btn = document.querySelector(".btn");
+const url = "https://api.adviceslip.com/advice";
+
+async function fetchAdvice() {
+
+    adviceNum.innerHTML = "...";
+    adivce.innerHTML = "wait a minute...";
+
+    try {
+        const fetchit = await fetch(url).then(res => res.json());
+
+        setTimeout(() => {
+            const adviceObj = fetchit.slip
+            const {advice, id } = adviceObj
+
+            adviceNum.innerHTML = `${id}`
+            adivce.innerHTML = advice
+        }, 1000)
+
+    } catch(error) {
+        adivce.innerHTML = " An unknown error occured"
+    }
+}
+
+btn.addEventListener("click", () => fetchAdvice())
